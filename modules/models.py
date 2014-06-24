48fa210c15fc5881f2c7190e612d58e17ec033a5
Brian Wu <brianwu02@gmail.com>
2014-06-24 00:36:53 -0400

    add JSON response method

--- modules/models.py
+++ modules/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime
 from database import Base
-from datetime import datetime, date
+from datetime import datetime, date 
+
+from flask import jsonify
 
 class Note(Base):
     __tablename__ = 'notes'
@@ -8,10 +10,24 @@ class Note(Base):
     note = Column(Text, nullable=False)
     timestamp = Column(DateTime, nullable=False)
 
+    def as_dict(self):
+        """return as dictionary"""
+        return {
+                'id': self.id,
+                'note': self.note,
+                'timestamp': self.timestamp
+                }
+
+    @classmethod
+    def jsonify_all(cls):
+        """Returns all Note instances as JSON Flask response"""
+        return jsonify(notes=[note.as_dict() for note in cls.query.all()])
+
     def __init__(self, note=None, timestamp=None):
         self.note = note
         self.timestamp = timestamp
 
     def __repr__(self):
         return "<Note(id='%s', note='%s', timestamp='%s')>" % (
-                self.id, self.note, self.timestamp) 
+                self.id, self.note, self.timestamp)
+
